232cce62a5e19e8bc93ac316f5892418eed444e7
Asim Zaidi <erasim.zaidi@gmail.com>
2022-10-18 00:16:16 +0530

    Updates

---
+++ precache-manifest.4af0cb051a0c729f64f70a96604a671e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac443a1493b89d14f1d6",
+    "url": "/erasim/my-cart/static/css/main.90962ef9.chunk.css"
+  },
+  {
+    "revision": "ac443a1493b89d14f1d6",
+    "url": "/erasim/my-cart/static/js/main.ac443a14.chunk.js"
+  },
+  {
+    "revision": "de53d44062204e8b07c9",
+    "url": "/erasim/my-cart/static/js/1.de53d440.chunk.js"
+  },
+  {
+    "revision": "a763f05ebff6a46bb87e",
+    "url": "/erasim/my-cart/static/js/2.a763f05e.chunk.js"
+  },
+  {
+    "revision": "cd458847b508524264f3",
+    "url": "/erasim/my-cart/static/js/runtime~main.cd458847.js"
+  },
+  {
+    "revision": "f14a32e71be2764b1a2ca8ad29ceaae7",
+    "url": "/erasim/my-cart/index.html"
+  }
+];
